319bf671f05f45aa6704ca977a2058053e000293
AndreyChulov <andreyc253@gmail.com>
2023-03-19 16:12:51 +0300

    Information about Client/Server dissconnection added

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Utilities;
 
@@ -14,32 +15,88 @@ namespace ChatClient
     {
         static void Main(string[] args)
         {
-            Socket socket = new Socket(SocketType.Stream, ProtocolType.IP);
-            socket.Connect(new IPEndPoint(IPAddress.Loopback, 10111));
+            var socket = ConnectClientToServer(new IPEndPoint(IPAddress.Loopback, 10111));
 
-            Console.WriteLine($"Clinet connected Local {socket.LocalEndPoint} Remote {socket.RemoteEndPoint}");
+            var chatContent = ReceiveChatContent(socket);
 
-            string chatContent = SocketUtility.ReceiveString(socket,
-                () =>
-                {
-                    Console.WriteLine($"Receive string size check from server client side exception");
-                }, () =>
-                {
-                    Console.WriteLine($"Receive string data check from server client side exception");
-                });
+            ShowChatContent(chatContent);
 
-            Console.WriteLine(chatContent);
+            var message = GetClientMessage();
+
+            SendMessageToServer(socket, message);
             
-            Console.Write("Your message:");
-            var message = Console.ReadLine();
-            SocketUtility.SendString(socket, message, () => {});
+            /*
+             * Потенциально буден нужна в ходе дальнейшей разработки
+             * В текущей версии строку ожидания Enter заменяет ожидание в
+             * 1 секунду ниже
+             */
+            //WaitForEnterPressedToCloseApplication();
 
-            Console.ReadLine();
+            DisconnectClientFromServer(socket);
+            
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            
+            DisposeClientSocket(socket);
+        }
 
+        private static void DisposeClientSocket(Socket socket)
+        {
             socket.Close();
             socket.Dispose();
+        }
 
+        private static void DisconnectClientFromServer(Socket socket)
+        {
+            socket.Disconnect(false);
+            Console.WriteLine("Client disconnected from server");
+        }
 
+        private static void WaitForEnterPressedToCloseApplication()
+        {
+            Console.Write("Press [Enter] to close client console application");
+            Console.ReadLine();
+        }
+
+        private static void SendMessageToServer(Socket socket, string message)
+        {
+            Console.WriteLine("Sending message to server");
+            SocketUtility.SendString(socket, message,
+                () => { Console.WriteLine($"Send string to server data check client side exception"); });
+            Console.WriteLine("Message sent to server");
+        }
+
+        private static string GetClientMessage()
+        {
+            Console.Write("Your message:");
+            var message = Console.ReadLine();
+            return message;
+        }
+
+        private static void ShowChatContent(string chatContent)
+        {
+            Console.WriteLine("---------------Chat content--------------------");
+            Console.WriteLine(chatContent);
+            Console.WriteLine("------------End of chat content----------------");
+            Console.WriteLine();
+        }
+
+        private static string ReceiveChatContent(Socket socket)
+        {
+            string chatContent = SocketUtility.ReceiveString(socket,
+                () => { Console.WriteLine($"Receive string size check from server client side exception"); },
+                () => { Console.WriteLine($"Receive string data check from server client side exception"); });
+            return chatContent;
+        }
+
+        private static Socket ConnectClientToServer(IPEndPoint serverEndPoint)
+        {
+            Socket socket = new Socket(SocketType.Stream, ProtocolType.IP);
+            
+            socket.Connect(serverEndPoint);
+
+            Console.WriteLine($"Client connected Local {socket.LocalEndPoint} Remote {socket.RemoteEndPoint}");
+            
+            return socket;
         }
     }
 }
--- ChatServer/ChatDatabase.cs
+++ ChatServer/ChatDatabase.cs
@@ -5,7 +5,13 @@ namespace ChatServer
 {
     internal static class ChatDatabase
     {
-        private static List<string> _chatLines = new List<string>() { "asasas", "asasas"};
+        private static List<string> _chatLines = new List<string>()
+        {
+            "Welcome to our best chat ever:", 
+            "----------------------------",
+            "Enter your joke now:", 
+            "----------------------------",
+        };
 
         public static void AddMessage(string message)
         {
