3e7c62b9e446d6b8bccd8a58370016159842f834
AndreyChulov <andreyc253@gmail.com>
2023-03-20 05:28:23 +0300

    Add buffer for receiving data

--- Utilities/SocketUtility.cs
+++ Utilities/SocketUtility.cs
@@ -13,35 +13,55 @@ namespace Utilities
             using (Stream dataStream = new MemoryStream())
             using (BinaryReader dataStreamReader = new BinaryReader(dataStream))
             {
-                var stringSize = ReceiveStringSize(clientSocket, dataStream, dataStreamReader, onReceiveDataSizeCheckFail);
-                return ReceiveString(clientSocket, stringSize + 1, dataStream, dataStreamReader, onReceiveDataCheckFail);
+                var dataSize = ReceiveDataSize(clientSocket, dataStream, dataStreamReader, onReceiveDataSizeCheckFail);
+                ReceiveDataToStream(clientSocket, dataSize, dataStream, onReceiveDataCheckFail);
+                
+                dataStream.Seek(0, SeekOrigin.Begin);
+                return dataStreamReader.ReadString();
             }
         }
         
-        private static string ReceiveString(Socket clientSocket, int stringSize, 
-            Stream dataStream, BinaryReader dataStreamReader, Action onReceiveDataCheckFail)
+        private static void ReceiveDataToStream(
+            Socket clientSocket, long dataSize, 
+            Stream dataStream, Action onReceiveDataCheckFail)
         {
-            WaitDataFromClient(clientSocket, stringSize);
+            var maxBufferSize = 1024;
+            var remainingDataSize = dataSize;
+
+            dataStream.Seek(0, SeekOrigin.Begin);
             
-            byte[] dataBuffer = new byte[stringSize];
-            var receivedBufferSize = clientSocket.Receive(dataBuffer);
-
-            if (receivedBufferSize != dataBuffer.Length)
+            while (remainingDataSize > maxBufferSize)
             {
-                onReceiveDataCheckFail();
-            }
+                ReceiveBufferToStream(clientSocket, dataStream, maxBufferSize, onReceiveDataCheckFail);
 
-            dataStream.Seek(0, SeekOrigin.Begin);
-            dataStream.Write(dataBuffer, 0, dataBuffer.Length);
-            dataStream.Seek(0, SeekOrigin.Begin);
-            return dataStreamReader.ReadString();
+                remainingDataSize -= maxBufferSize;
+            }
+            
+            ReceiveBufferToStream(clientSocket, dataStream, (int)remainingDataSize, onReceiveDataCheckFail);
         }
 
-        private static int ReceiveStringSize(Socket clientSocket, Stream dataStream, 
+        private static void ReceiveBufferToStream(
+            Socket clientSocket, Stream dataStream, int bufferSize,
+            Action onReceiveDataCheckFail)
+        {
+            WaitDataFromClient(clientSocket, bufferSize);
+
+            byte[] dataBuffer = new byte[bufferSize];
+            var receivedBufferSize = clientSocket.Receive(dataBuffer);
+
+            if (receivedBufferSize != bufferSize)
+            {
+                onReceiveDataCheckFail();
+            }
+
+            dataStream.Write(dataBuffer, 0, bufferSize);
+        }
+
+        private static long ReceiveDataSize(Socket clientSocket, Stream dataStream, 
             BinaryReader dataStreamReader, Action onReceiveDataCheckFail)
         {
-            WaitDataFromClient(clientSocket, sizeof(int));
-            byte[] dataBuffer = new byte[sizeof(int)];
+            WaitDataFromClient(clientSocket, sizeof(long));
+            byte[] dataBuffer = new byte[sizeof(long)];
             var receivedBufferSize = clientSocket.Receive(dataBuffer);
 
             if (receivedBufferSize != dataBuffer.Length)
@@ -52,7 +72,7 @@ namespace Utilities
             dataStream.Seek(0, SeekOrigin.Begin);
             dataStream.Write(dataBuffer, 0, dataBuffer.Length);
             dataStream.Seek(0, SeekOrigin.Begin);
-            return dataStreamReader.ReadInt32();
+            return dataStreamReader.ReadInt64();
         }
 
         public static void WaitDataFromClient(Socket clientSocket)
@@ -73,14 +93,25 @@ namespace Utilities
             using (Stream dataStream = new MemoryStream())
             using (BinaryWriter dataStreamWriter = new BinaryWriter(dataStream))
             {
-                dataStreamWriter.Write(dataToSend.Length);
+                /*
+                 * записываем пустышку вместо размера пакета данных,
+                 * на данном этапе мы не знаем размер отправляемых данных
+                 */
+                dataStreamWriter.Write((long)0);
+                
                 dataStreamWriter.Write(dataToSend);
                 dataStreamWriter.Flush();
             
                 byte[] sendDataBuffer = new byte[dataStream.Position];
 
+                /*
+                 * Перезаписываем актуальный размер пакета данных,
+                 * теперь мы знаем его размер
+                 */
                 dataStream.Seek(0, SeekOrigin.Begin);
+                dataStreamWriter.Write(dataStream.Length - sizeof(long));
                 
+                dataStream.Seek(0, SeekOrigin.Begin);
                 int readBytesFromMemoryStream = dataStream.Read(sendDataBuffer, 0, sendDataBuffer.Length);
 
                 if (readBytesFromMemoryStream != sendDataBuffer.Length)
